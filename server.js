a4ede97d285fcf0c1515c305b548d7dd5b53d2c9
keerthanarajendran123 <keerthurb9@gmail.com>
2024-04-06 20:09:37 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: ['https://blood-bank-frontend97.netlify.app/', 'http://localhost:5173']
+}));
 
 // Routes
 import testRoutes from "./routes/testRoutes.js";
